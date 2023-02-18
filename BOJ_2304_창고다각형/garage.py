c94790925b46ecf48c640e66a7971502725fb4ee
heejinssss <heejin.dev33@gmail.com>
2023-02-19 00:27:04 +0900

    시간 줄여야 함

--- BOJ_2304_창고다각형/garage.py
+++ BOJ_2304_창고다각형/garage.py
@@ -6,96 +6,49 @@ lst = [list(map(int, input().split())) for _ in range(N)] # 기둥 좌측 상단
 
 lst.sort() # 기둥을 x축 기준으로 순서대로 정렬
 
-sum = 0
-l = 0
-r = N-1
+max_ = lst[0][1]
+idx = 0
+for i in range(N):
+    if max_ < lst[i][1]:
+        max_ = lst[i][1]
+        idx = i
+
+std = lst[idx][1] # 제일 높은 기둥(기준) 넓이
+
+idx_ll = idx_rr = idx
+idx_l = idx_r = idx
+sum_l = 0
+sum_r = 0
 
 while True:
-    # 왼쪽 기둥보다 오른쪽 기둥이 크다면
-    if lst[l][1] <= lst[l+1][1]:
-        sum += (lst[l+1][0] - lst[l][0]) * lst[l][1]
-        l += 1
-    # 왼쪽 기둥보다 오른쪽 기둥이 작다면? 큰 기둥이 나올 때까지 찾아야지
-    else:
-        i = 1
-        done = False
-        while True:
-            # 왼쪽 기둥보다 큰 오른쪽 기둥이 나오면 중지
-            if lst[l][1] <= lst[l+i][1]:
-                done = True
-                break
-
-            i += 1
-
-            # 왼쪽 기둥보다 큰 오른쪽 기둥이 없다면, 그 중에서 큰 기둥을 찾아야지
-            if l+i > N-1:
-                temp = lst[l+1:]
-                max_ = temp[0][1]
-                max_idx = 0
-                for k in range(1, len(temp)):
-                    if max_ <= temp[k][1]:
-                        max_ = temp[k][1]
-                        max_idx = k
-                sum += lst[l][1] + (temp[max_idx][0]-lst[l][0]-1) * temp[max_idx][1]
-                l += max_idx+1
-                break
-
-        # 왼쪽 기둥보다 큰 오른쪽 기둥을 찾았다면
-        if done:
-            sum += (lst[l + i][0] - lst[l][0]) * lst[l][1]
-            l = l+i
-
-    if r == l:
-        sum += lst[l][1]
-        print(sum)
-        break
-    elif l > 5:
-        print(sum)
-        break
-    elif r < 0:
-        print(sum)
+    if idx_ll == 0:
         break
 
-    # 오른쪽 기둥보다 왼쪽 기둥이 크다면
-    if lst[r-1][1] >= lst[r][1]:
-        sum += (lst[r][0] - lst[r-1][0]) * lst[r][1]
-        r -= 1
-    else:
-        j = -1
-        done = False
-        while True:
-            # 오른쪽 기둥보다 큰 왼쪽 기둥이 나오면 중지
-            if lst[r+j][1] >= lst[r][1]:
-                done = True
-                break
+    temp_l = lst[:idx_ll]
+    max_l = temp_l[0][1]
+    idx_l = 0
+    for i in range(len(temp_l)):
+        if max_l <= temp_l[i][1]:
+            max_l = temp_l[i][1]
+            idx_l = i
 
-            j -= 1
+    sum_l += abs(lst[idx_ll][0]-temp_l[idx_l][0])*temp_l[idx_l][1]
+    idx_ll = idx_l
 
-            # 왼쪽 기둥보다 큰 오른쪽 기둥이 없다면, 그 중에서 큰 기둥을 찾아야지
-            if r+j < 0:
-                temp = lst[:r]
-                max_ = temp[0][1]
-                max_idx = 0
-                for k in range(1, len(temp)):
-                    if max_ <= temp[k][1]:
-                        max_ = temp[k][1]
-                        max_idx = k
-                sum += lst[r][1] + (lst[r][0]-temp[max_idx][0]-1) * temp[max_idx][1]
-                r = max_idx
-                break
-
-        # 오른쪽 기둥보다 큰 왼쪽 기둥을 찾았다면
-        if done:
-            sum += (lst[r][0]-lst[r+j][0]) * lst[r][1]
-            r = r+j
-
-    if r == l:
-        sum += lst[l][1]
-        print(sum)
+while True:
+    if idx_rr == N-1:
         break
-    elif l > 5:
-        print(sum)
-        break
-    elif r < 0:
-        print(sum)
-        break
+
+    temp_r = lst[idx_rr+1:]
+    max_r = temp_r[0][1]
+    idx_r = 0
+    for i in range(len(temp_r)):
+        if max_r <= temp_r[i][1]:
+            max_r = temp_r[i][1]
+            idx_r = i
+
+    sum_r += abs(lst[idx_rr][0]-temp_r[idx_r][0])*temp_r[idx_r][1]
+    idx_rr = idx_rr+idx_r+1
+
+ans = std+sum_l+sum_r
+print(ans)
